337a0b4ff87f1690a9b4fbc9c069b56e93a7a800
Bea <bdcarroll0@gmail.com>
2019-04-01 19:07:13 +0100

    css

--- app.py
+++ app.py
@@ -31,4 +31,11 @@ if __name__ =="__main__":
 def contact():
     return render_template("contact.html")
 
+
+'''
+@app.route("/<path:path>")
+def static_file(path):
+    return app.send_static_file(path)
+'''
+
 # })
